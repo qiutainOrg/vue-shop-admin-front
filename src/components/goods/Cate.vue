a9ed5a48fb19ee589cf8eb70a5770d9232bdd40f
qiutian00 <1476102147@qq.com>
2021-01-02 12:40:55 +0800

    fix: 修改本地的接口地址

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -3,8 +3,8 @@
     <!-- 面包屑导航区 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
-      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+      <el-breadcrumb-item>学生管理</el-breadcrumb-item>
+      <el-breadcrumb-item>学生分类</el-breadcrumb-item>
     </el-breadcrumb>
     <!-- 卡片视图 -->
     <el-card>
--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -3,8 +3,8 @@
     <!-- 面包屑导航区 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
-      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+      <el-breadcrumb-item>学生管理</el-breadcrumb-item>
+      <el-breadcrumb-item>学生列表</el-breadcrumb-item>
     </el-breadcrumb>
     <!-- 卡片视图 -->
     <el-card>
